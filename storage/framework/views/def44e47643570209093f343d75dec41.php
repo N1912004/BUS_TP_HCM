e0c580aae839481803cfdd70c4174709d955936f
Quoc Nghi <you@example.com>
2025-11-24 18:39:12 +0700

    Clean repo and remove Laravel cache

--- storage/framework/views/def44e47643570209093f343d75dec41.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/laravelversion1.com/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
